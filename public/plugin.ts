5358b26be9da3a7bccfaef56a74f7fd4cff2759e
tehbooom <onlyboomm@gmail.com>
2023-02-28 12:01:14 -0500

    Changed logo and cleaned up file

--- public/plugin.ts
+++ public/plugin.ts
@@ -5,25 +5,20 @@ import {
   MitreAttackPluginStart,
   AppPluginStartDependencies,
 } from './types';
-import { PLUGIN_NAME, PLUGIN_ID } from '../common';
+import { PLUGIN_NAME } from '../common';
 
 export class MitreAttackPlugin implements Plugin<MitreAttackPluginSetup, MitreAttackPluginStart> {
   public setup(core: CoreSetup): MitreAttackPluginSetup {
-    const mitreLogo: string = `${core.http.basePath.get()}/plugins/${PLUGIN_ID}/assets/`;
-
     core.application.register({
       id: 'mitreAttack',
       title: PLUGIN_NAME,
-      euiIconType: 'logoKibana',
+      euiIconType: 'tableDensityNormal',
       async mount(params: AppMountParameters) {
         const { renderApp } = await import('./application');
-        // Get start services as specified in kibana.json
         const [coreStart, depsStart] = await core.getStartServices();
         return renderApp(coreStart, depsStart as AppPluginStartDependencies, params);
       },
     });
-
-    // Return methods that should be available to other plugins
     return {};
   }
 
